f5b500f9d4da2f1af2033659e22fc5ba3fc8f9a9
Zinath22 <Zinath22@users.noreply.github.com>
2024-01-18 22:25:51 +0600

    contact form add

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -15,7 +15,43 @@ const Contact = () => {
 
 
 
-        {/* <div className=" w-full max-w-sm  ">
+    <div className="flex text-purple-600 justify-start space-y-4 flex-col">
+  <form>
+    <input className="border p-1 mb-3 border-purple-600" placeholder="Name" type="text" name="name" id="name" />
+    <br />
+    <input className="border mb-3 p-1 border-purple-600" placeholder="Email" type="email" name="email" id="email" />
+    <br />
+    <textarea
+      placeholder="Your Message"
+      className="border mb-3 p-2 border-purple-600"
+      cols="30"
+      rows="3"
+    ></textarea>
+    <br />
+    <button type="submit" className="btn bg-purple-600 text-white">Submit</button>
+  </form>
+</div>
+
+        <div className="text-center lg:text-left mb-5">
+            <div className="text-left  space-y-5 mb-6">
+           
+            <p className="flex items-center gap-4"><FaEnvelope className="text-purple-600"/>zinathfarhana22@gmail.com</p>
+            <p className="flex items-center gap-4"><FaLocationArrow className="text-purple-600"/> Rajshahi,Bangladesh</p>
+
+            </div>
+            {/* <p className="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p> */}
+            
+        </div>
+    </div>
+</div>
+</div>
+    );
+};
+
+export default Contact;
+
+
+  {/* <div className=" w-full max-w-sm  ">
             <form className="space-y-2">
                 <div className="form-control">
 
@@ -37,34 +73,3 @@ const Contact = () => {
                 </div>
             </form>
         </div> */}
-         <div className="flex text-purple-600 justify-start  space-y-4 flex-col">
-  
-  <input className="border p-1 border-purple-600" placeholder="Name" type="name" name="" id="" />
-<input className="border p-1 border-purple-600" placeholder="Email" type="email" name="" id="" />
-<textarea
-placeholder="Your Message"
-   className="border p-2 border-purple-600"
-   cols="30"
-   rows="3"
- ></textarea>
-<button className="btn bg-purple-600 text-white">Submit</button>
-
-
-</div>
-        <div className="text-center lg:text-left mb-5">
-            <div className="text-left  space-y-5 mb-6">
-           
-            <p className="flex items-center gap-4"><FaEnvelope className="text-purple-600"/>zinathfarhana22@gmail.com</p>
-            <p className="flex items-center gap-4"><FaLocationArrow className="text-purple-600"/> Rajshahi,Bangladesh</p>
-
-            </div>
-            {/* <p className="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p> */}
-            
-        </div>
-    </div>
-</div>
-</div>
-    );
-};
-
-export default Contact;
